edbab060c39039f2fcd735c575c75c36715d0f5d
npchitman <npchitman@github.com>
2021-01-17 21:24:10 +0800

    项目初始化

---
+++ Hazel/include/Test.h
@@ -0,0 +1,12 @@
+//
+// Created by Npchitman on 2021/1/17.
+//
+
+#ifndef HAZELENGINE_TEST_H
+#define HAZELENGINE_TEST_H
+
+namespace Hazel{
+    __declspec(dllexport) void Print();
+}
+
+#endif //HAZELENGINE_TEST_H
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Npchitman on 2021/1/17.
+//
+namespace Hazel{
+    __declspec(dllimport) void Print();
+}
+
+int main(){
+    Hazel::Print();
+}
