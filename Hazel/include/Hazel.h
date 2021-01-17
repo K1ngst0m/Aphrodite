9a977e1ad3fac23b405dad3db451c6051561239b
npchitman <npchitman@github.com>
2021-01-18 01:19:01 +0800

    初始化引擎头文件

---
+++ Hazel/include/Hazel.h
@@ -0,0 +1,18 @@
+//
+// Created by Npchitman on 2021/1/17.
+//
+
+#ifndef HAZELENGINE_HAZEL_H
+#define HAZELENGINE_HAZEL_H
+
+//For use by Hazel applications
+#include <cstdio>
+#include "Application.h"
+#include "Log.h"
+
+// ---Entry Point--------------------------
+#include "EntryPoint.h"
+// ----------------------------------------
+
+
+#endif //HAZELENGINE_HAZEL_H
