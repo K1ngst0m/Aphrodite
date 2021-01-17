a74f257b155892ed100841e0e460f896886122e5
npchitman <npchitman@github.com>
2021-01-18 01:17:40 +0800

    添加日志库

---
+++ Hazel/include/Log.h
@@ -0,0 +1,41 @@
+//
+// Created by Npchitman on 2021/1/18.
+//
+
+#ifndef HAZELENGINE_LOG_H
+#define HAZELENGINE_LOG_H
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Hazel{
+    class HAZEL_API Log{
+    public:
+        static void Init();
+
+        inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() {return s_CoreLogger;}
+        inline static std::shared_ptr<spdlog::logger>& GetClientLogger() {return s_ClientLogger;}
+
+    private:
+        static std::shared_ptr<spdlog::logger> s_CoreLogger;
+        static std::shared_ptr<spdlog::logger> s_ClientLogger;
+    };
+}
+
+// Core log macros
+#define HZ_CORE_TRACE(...)          ::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HZ_CORE_INFO(...)           ::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HZ_CORE_WARN(...)           ::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HZ_CORE_ERROR(...)          ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HZ_CORE_FATAL(...)          ::Hazel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define HZ_TRACE(...)          ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HZ_INFO(...)           ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HZ_WARN(...)           ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HZ_ERROR(...)          ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HZ_FATAL(...)          ::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
+#endif //HAZELENGINE_LOG_H
---
+++ Hazel/src/Hazel/Log.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Npchitman on 2021/1/18.
+//
+
+#include "Log.h"
+
+namespace Hazel{
+    std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+    std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+    void Log::Init() {
+        spdlog::set_pattern("%^[%T] %n: %v%$");
+        s_CoreLogger = spdlog::stderr_color_mt("Hazel");
+        s_CoreLogger->set_level(spdlog::level::trace);
+
+        s_ClientLogger = spdlog::stderr_color_mt("APP");
+        s_ClientLogger->set_level(spdlog::level::trace);
+    }
+}
