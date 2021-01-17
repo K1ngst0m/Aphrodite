c944a16befd69c39b168e2ecec848c4f7cbd94cb
npchitman <npchitman@github.com>
2021-01-18 01:19:54 +0800

    初始化入口

---
+++ Hazel/include/EntryPoint.h
@@ -0,0 +1,22 @@
+//
+// Created by Npchitman on 2021/1/17.
+//
+
+#ifndef HAZELENGINE_ENTRYPOINT_H
+#define HAZELENGINE_ENTRYPOINT_H
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char ** argv){
+    Hazel::Log::Init();
+    HZ_CORE_WARN("Initialized Log!");
+    HZ_INFO("Hello!");
+    auto app = Hazel::CreateApplication();
+    app->Run();
+    delete app;
+}
+
+#endif
+#endif //HAZELENGINE_ENTRYPOINT_H
