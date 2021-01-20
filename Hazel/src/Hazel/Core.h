51254b2723cdcd9ea956b0b8e8d9d65397f0b7b7
npchitman <npchitman@github.com>
2021-01-20 18:23:22 +0800

    添加断言工具

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -15,6 +15,14 @@
     #error  Hazel only supports Windows!
 #endif
 
+#ifdef HZ_ENABLE_ASSERTS
+    #define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }
+    #define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+    #define HZ_ASSERT(x, ...)
+    #define HZ_CORE_ASSERT(x, ...)
+#endif
+
 #define BIT(x) (1 << x)
 
 #endif //HAZELENGINE_CORE_H
