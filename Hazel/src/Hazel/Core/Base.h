0554c45708349f0cb7e4ca4b94777eb237dcb833
npchitman <npchitman@outlook.com>
2021-06-28 02:25:46 +0800

    - fix entity handle invalid

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -67,7 +67,7 @@
 
 #define BIT(x) (1 << x)
 
-#define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Hazel {
     template<typename T>
--- Hazel/src/Hazel/Scene/Entity.h
+++ Hazel/src/Hazel/Scene/Entity.h
@@ -42,10 +42,10 @@ namespace Hazel {
             m_Scene->m_Registry.remove<T>(m_EntityHandle);
         }
 
-        explicit operator bool() const { return static_cast<int>(m_EntityHandle) != 0; }
+        explicit operator bool() const { return m_EntityHandle != entt::null; }
 
     private:
-        entt::entity m_EntityHandle{0};
+        entt::entity m_EntityHandle{entt::null};
         Scene* m_Scene = nullptr;
     };
 }// namespace Hazel
