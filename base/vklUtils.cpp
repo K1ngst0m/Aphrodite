a02b1ba378385da5eb138e8992c1d89ce48baf85
Kingstom <kingstom.chen@gmail.com>
2022-08-23 15:41:25 +0800

    restore swapchain colorspace

--- base/vklUtils.cpp
+++ base/vklUtils.cpp
@@ -58,11 +58,10 @@ std::vector<char> readFile(const std::string &filename)
     return buffer;
 }
 
-
 VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats)
 {
     for (const auto &availableFormat : availableFormats) {
-        if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM &&
+        if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
             availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
             return availableFormat;
         }
