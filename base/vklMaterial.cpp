7395c6ee3c81b0e8a927b92bb23d166226616a84
Kingstom <kingstom.chen@amd.com>
2022-08-31 11:09:41 +0800

    delete unsed function and member

--- base/vklMaterial.cpp
+++ base/vklMaterial.cpp
@@ -1,48 +1,5 @@
 #include "vklMaterial.h"
 
 namespace vkl {
-void Material::createDescriptorSet(VkDevice device, VkDescriptorPool descriptorPool, VkDescriptorSetLayout descriptorSetLayout)
-{
-    assert(baseColorTexture);
-    assert(specularTexture);
 
-    VkDescriptorSetAllocateInfo descriptorSetAllocInfo{
-        .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
-        .descriptorPool = descriptorPool,
-        .descriptorSetCount = 1,
-        .pSetLayouts = &descriptorSetLayout,
-    };
-    VK_CHECK_RESULT(vkAllocateDescriptorSets(device, &descriptorSetAllocInfo, &descriptorSet));
-
-    std::vector<VkDescriptorImageInfo> imageDescriptors{};
-    std::vector<VkWriteDescriptorSet> writeDescriptorSets{};
-
-    if (baseColorTexture){
-        imageDescriptors.push_back(baseColorTexture->descriptorInfo);
-        VkWriteDescriptorSet writeDescriptorSet{
-            .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-            .dstSet = descriptorSet,
-            .dstBinding = static_cast<uint32_t>(writeDescriptorSets.size()),
-            .descriptorCount = 1,
-            .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-            .pImageInfo = &baseColorTexture->descriptorInfo,
-        };
-        writeDescriptorSets.push_back(writeDescriptorSet);
-    }
-
-    if (specularTexture){
-        imageDescriptors.push_back(specularTexture->descriptorInfo);
-        VkWriteDescriptorSet writeDescriptorSet{
-            .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-            .dstSet = descriptorSet,
-            .dstBinding = static_cast<uint32_t>(writeDescriptorSets.size()),
-            .descriptorCount = 1,
-            .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-            .pImageInfo = &specularTexture->descriptorInfo,
-        };
-        writeDescriptorSets.push_back(writeDescriptorSet);
-    }
-
-    vkUpdateDescriptorSets(device, static_cast<uint32_t>(writeDescriptorSets.size()), writeDescriptorSets.data(), 0, nullptr);
-}
 }
--- base/vklMaterial.h
+++ base/vklMaterial.h
@@ -10,15 +10,9 @@ namespace vkl {
 struct Material {
     glm::vec4 baseColorFactor = glm::vec4(1.0f);
     uint32_t baseColorTextureIndex = 0;
-    glm::vec4 specularFactor = glm::vec4(1.0f);
-    float shininess = 64.0f;
 
     vkl::Texture *baseColorTexture = nullptr;
     vkl::Texture *specularTexture = nullptr;
-
-    VkDescriptorSet descriptorSet;
-
-    void createDescriptorSet(VkDevice device, VkDescriptorPool descriptorPool, VkDescriptorSetLayout descriptorSetLayout);
 };
 }
 
