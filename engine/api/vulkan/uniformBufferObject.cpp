c0fd1f0994aedd34c28c29e9f87350df623dfca3
Kingstom <kingstom.chen@gmail.com>
2022-09-24 20:28:13 +0800

    use persistent map in uniform buffer

--- engine/api/vulkan/uniformBufferObject.cpp
+++ engine/api/vulkan/uniformBufferObject.cpp
@@ -3,9 +3,7 @@
 namespace vkl {
 
 void VulkanUniformBufferObject::updateBuffer(void *data) {
-    buffer.map();
     buffer.copyTo(data, buffer.size);
-    buffer.unmap();
 }
 VulkanUniformBufferObject::VulkanUniformBufferObject(vkl::SceneRenderer *renderer, vkl::VulkanDevice *device, vkl::UniformBufferObject *ubo)
     : _device(device), _renderer(renderer), _ubo(ubo) {
@@ -15,6 +13,7 @@ void VulkanUniformBufferObject::setupBuffer(uint32_t bufferSize, void *data) {
     _device->createBuffer(bufferSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
                          VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, buffer, data);
     buffer.setupDescriptor();
+    buffer.map();
 }
 
 void VulkanUniformBufferObject::cleanupResources() {
