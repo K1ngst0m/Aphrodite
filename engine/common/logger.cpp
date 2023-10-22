6e2f3bd359a9440ec6216cf936192732115f5969
K1ngst0m <kingstom.chen@gmail.com>
2023-10-22 12:23:42 +0800

    refine singleton logger

--- engine/common/logger.cpp
+++ engine/common/logger.cpp
@@ -1,9 +1,5 @@
 #include "logger.h"
 
-#include <reckless/severity_log.hpp>
-#include <reckless/file_writer.hpp>
-#include <reckless/stdout_writer.hpp>
-
 namespace aph
 {
 }  // namespace aph
--- engine/common/logger.h
+++ engine/common/logger.h
@@ -18,71 +18,108 @@ private:
     Logger() : m_fileWriter{"log.txt"}, m_logger{&m_stdcoutWriter} {}
 
 public:
-    static Logger* g_logger;
-    static log_t*  Get()
+    // Delete copy constructor and assignment operator
+    Logger(Logger const&)                  = delete;
+    Logger&       operator=(Logger const&) = delete;
+    static Logger& Get()
     {
-        if(g_logger == nullptr)
-        {
-            g_logger = new Logger();
-        }
-        return &g_logger->m_logger;
+        static Logger instance;
+        return instance;
     }
+
+public:
+    void flush() {}
+
+    template <typename... Args>
+    void debug(std::string_view fmt, Args&&... args);
+
+    template <typename... Args>
+    void warn(std::string_view fmt, Args&&... args);
+
+    template <typename... Args>
+    void info(std::string_view fmt, Args&&... args);
+
+    template <typename... Args>
+    void error(std::string_view fmt, Args&&... args);
 
 private:
     reckless::file_writer   m_fileWriter{"log.txt"};
     reckless::stdout_writer m_stdcoutWriter{};
     log_t                   m_logger;
 };
-inline Logger* Logger::g_logger = nullptr;
+
+template <typename... Args>
+void Logger::debug(std::string_view fmt, Args&&... args)
+{
+    m_logger.debug(fmt.data(), std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+void Logger::warn(std::string_view fmt, Args&&... args)
+{
+    m_logger.warn(fmt.data(), std::forward<Args>(args)...);
+}
+template <typename... Args>
+void Logger::info(std::string_view fmt, Args&&... args)
+{
+    m_logger.info(fmt.data(), std::forward<Args>(args)...);
+}
+template <typename... Args>
+void Logger::error(std::string_view fmt, Args&&... args)
+{
+    m_logger.error(fmt.data(), std::forward<Args>(args)...);
+}
 }  // namespace aph
+
+
 
 #define LOG_FLUSH() \
     do \
     { \
-        ::aph::Logger::Get()->flush(); \
+        ::aph::Logger::Get().flush(); \
     } while(0)
 
 #define CM_LOG_DEBUG(...) \
     do \
     { \
-        ::aph::Logger::Get()->debug("[APH] " __VA_ARGS__); \
+        ::aph::Logger::Get().debug("[APH] " __VA_ARGS__); \
     } while(0)
 #define CM_LOG_WARN(...) \
     do \
     { \
-        ::aph::Logger::Get()->warn("[APH] " __VA_ARGS__); \
+        ::aph::Logger::Get().warn("[APH] " __VA_ARGS__); \
     } while(0)
 #define CM_LOG_INFO(...) \
     do \
     { \
-        ::aph::Logger::Get()->info("[APH] " __VA_ARGS__); \
+        ::aph::Logger::Get().info("[APH] " __VA_ARGS__); \
     } while(0)
 #define CM_LOG_ERR(...) \
     do \
     { \
-        ::aph::Logger::Get()->error("[APH] " __VA_ARGS__); \
+        ::aph::Logger::Get().error("[APH] " __VA_ARGS__); \
     } while(0)
 
 #define VK_LOG_DEBUG(...) \
     do \
     { \
-        ::aph::Logger::Get()->debug("[VK] " __VA_ARGS__); \
+        ::aph::Logger::Get().debug("[VK] " __VA_ARGS__); \
     } while(0)
 #define VK_LOG_WARN(...) \
     do \
     { \
-        ::aph::Logger::Get()->warn("[VK] " __VA_ARGS__); \
+        ::aph::Logger::Get().warn("[VK] " __VA_ARGS__); \
     } while(0)
 #define VK_LOG_INFO(...) \
     do \
     { \
-        ::aph::Logger::Get()->info("[VK] " __VA_ARGS__); \
+        ::aph::Logger::Get().info("[VK] " __VA_ARGS__); \
     } while(0)
 #define VK_LOG_ERR(...) \
     do \
     { \
-        ::aph::Logger::Get()->error("[VK] " __VA_ARGS__); \
-        ::aph::Logger::Get()->flush(); \
+        ::aph::Logger::Get().error("[VK] " __VA_ARGS__); \
+        ::aph::Logger::Get().flush(); \
     } while(0)
 
 #endif  // LOGGER_H_
