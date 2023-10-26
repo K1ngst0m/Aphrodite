e3144e1ee749dc383d9f3913269443fe57fdabc2
K1ngst0m <kingstom.chen@gmail.com>
2023-10-26 22:20:05 +0800

    init module

---
+++ engine/module/module.h
@@ -0,0 +1,43 @@
+#ifndef MODULE_H_
+#define MODULE_H_
+
+namespace aph
+{
+class Module
+{
+public:
+    Module() = default;
+    explicit Module(const char* path);
+    ~Module();
+
+    Module(Module&& other) noexcept;
+    Module& operator=(Module&& other) noexcept;
+
+    template <typename Func>
+    Func getSymbol(const char* symbol)
+    {
+        return reinterpret_cast<Func>(getSymbolInternal(symbol));
+    }
+
+    explicit operator bool() const
+    {
+#if _WIN32
+        return m_module != nullptr;
+#else
+        return m_dylib != nullptr;
+#endif
+    }
+
+private:
+#if _WIN32
+    HMODULE m_module = nullptr;
+#else
+    void* m_dylib = nullptr;
+#endif
+
+    void* getSymbolInternal(const char* symbol);
+    void  close();
+};
+}  // namespace aph
+
+#endif  // MODULE_H_
