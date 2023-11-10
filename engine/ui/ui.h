3f1b668de26c8937dafe4e6a75692e083103aa65
K1ngst0m <kingstom.chen@gmail.com>
2023-11-10 22:45:03 +0800

    UI: cmake target setup

--- engine/ui/ui.h
+++ engine/ui/ui.h
@@ -1,22 +1,18 @@
 #ifndef VULKAN_UIRENDERER_H_
 #define VULKAN_UIRENDERER_H_
 
-#include "renderer/renderer.h"
+#include "math/math.h"
 
-namespace aph::ui
+namespace aph
 {
-void drawWithItemWidth(float itemWidth, float scale, std::function<void()>&& drawFunc);
-void drawWindow(std::string_view title, glm::vec2 pos, glm::vec2 size, float scale, std::function<void()>&& drawFunc);
-bool header(const char* caption);
-bool checkBox(const char* caption, bool* value);
-bool checkBox(const char* caption, int32_t* value);
-bool radioButton(const char* caption, bool value);
-bool sliderFloat(const char* caption, float* value, float min, float max);
-bool sliderInt(const char* caption, int32_t* value, int32_t min, int32_t max);
-bool comboBox(const char* caption, int32_t* itemindex, std::vector<std::string> items);
-bool button(const char* caption);
-bool colorPicker(const char* caption, float* color);
-void text(const char* formatstr, ...);
-}  // namespace aph::ui
+struct UIComponentDesc
+{
+    vec2 offset = {0.0f, 150.0f};
+    vec2 size   = {600.0f, 550.0f};
+
+    uint32_t fontID   = 0;
+    float    fontSize = 16.0f;
+};
+}  // namespace aph
 
 #endif  // UIRENDERER_H_
