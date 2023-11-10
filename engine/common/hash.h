ac150e6c82d2149ed60a0c78eef2ce739889459e
K1ngst0m <kingstom.chen@gmail.com>
2023-11-10 22:38:12 +0800

    hashmap/set fallback for debugging

--- engine/common/hash.h
+++ engine/common/hash.h
@@ -5,6 +5,7 @@
 
 namespace aph
 {
+#if 1
 template <class Key,
           class T,
           class Hash = ::ankerl::unordered_dense::hash<Key>,
@@ -19,6 +20,12 @@ template <class Key,
           class AllocatorOrContainer = std::allocator<Key>,
           class Bucket = ::ankerl::unordered_dense::bucket_type::standard>
 using HashSet = ::ankerl::unordered_dense::set<Key, Hash, KeyEqual, AllocatorOrContainer, Bucket>;
+#else
+template <class Key, class Hash>
+using HashMap = std::unordered_map<Key, Hash>;
+template <class Key>
+using HashSet = std::unordered_set<Key>;
+#endif
 }  // namespace aph
 
 #endif
