1a8d13110e9b3a81e94e9a223f74235decdf6a66
Kingstom <kingstom.chen@amd.com>
2023-11-21 17:03:49 +0800

    move pipeline builder to source file

--- engine/api/vulkan/pipeline.cpp
+++ engine/api/vulkan/pipeline.cpp
@@ -5,6 +5,62 @@
 
 namespace aph::vk
 {
+
+class VulkanPipelineBuilder final
+{
+public:
+    VulkanPipelineBuilder();
+    ~VulkanPipelineBuilder() = default;
+
+    VulkanPipelineBuilder& depthBiasEnable(bool enable);
+    VulkanPipelineBuilder& dynamicState(VkDynamicState state);
+    VulkanPipelineBuilder& primitiveTopology(VkPrimitiveTopology topology);
+    VulkanPipelineBuilder& rasterizationSamples(VkSampleCountFlagBits samples);
+    VulkanPipelineBuilder& shaderStage(VkPipelineShaderStageCreateInfo stage);
+    VulkanPipelineBuilder& shaderStage(const SmallVector<VkPipelineShaderStageCreateInfo>& stages);
+    VulkanPipelineBuilder& stencilStateOps(VkStencilFaceFlags faceMask, VkStencilOp failOp, VkStencilOp passOp,
+                                           VkStencilOp depthFailOp, VkCompareOp compareOp);
+    VulkanPipelineBuilder& stencilMasks(VkStencilFaceFlags faceMask, uint32_t compareMask, uint32_t writeMask,
+                                        uint32_t reference);
+    VulkanPipelineBuilder& cullMode(VkCullModeFlags mode);
+    VulkanPipelineBuilder& frontFace(VkFrontFace mode);
+    VulkanPipelineBuilder& polygonMode(VkPolygonMode mode);
+    VulkanPipelineBuilder& vertexInputState(const VkPipelineVertexInputStateCreateInfo& state);
+    VulkanPipelineBuilder& colorAttachments(const VkPipelineColorBlendAttachmentState* states, const VkFormat* formats,
+                                            uint32_t numColorAttachments);
+    VulkanPipelineBuilder& depthAttachmentFormat(VkFormat format);
+    VulkanPipelineBuilder& stencilAttachmentFormat(VkFormat format);
+
+    VkResult build(Device* pDevice, VkPipelineCache pipelineCache, VkPipelineLayout pipelineLayout,
+                   VkPipeline* outPipeline) noexcept;
+
+    static uint32_t getNumPipelinesCreated() { return numPipelinesCreated_; }
+
+private:
+    enum
+    {
+        APH_MAX_DYNAMIC_STATES = 128
+    };
+    uint32_t       numDynamicStates_                      = 0;
+    VkDynamicState dynamicStates_[APH_MAX_DYNAMIC_STATES] = {};
+
+    SmallVector<VkPipelineShaderStageCreateInfo> shaderStages_ = {};
+
+    VkPipelineVertexInputStateCreateInfo   vertexInputState_;
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly_;
+    VkPipelineRasterizationStateCreateInfo rasterizationState_;
+    VkPipelineMultisampleStateCreateInfo   multisampleState_;
+    VkPipelineDepthStencilStateCreateInfo  depthStencilState_;
+
+    SmallVector<VkPipelineColorBlendAttachmentState> colorBlendAttachmentStates_ = {};
+    SmallVector<VkFormat>                            colorAttachmentFormats_     = {};
+
+    VkFormat depthAttachmentFormat_   = VK_FORMAT_UNDEFINED;
+    VkFormat stencilAttachmentFormat_ = VK_FORMAT_UNDEFINED;
+
+    static uint32_t numPipelinesCreated_;
+};
+
 uint32_t VulkanPipelineBuilder::numPipelinesCreated_ = 0;
 
 VulkanPipelineBuilder::VulkanPipelineBuilder() :
