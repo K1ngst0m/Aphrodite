5dcd6f4f19f13b90dfe868ad23c1e0fa27d5c6e9
K1ngst0m <kingstom.chen@gmail.com>
2024-01-14 21:19:04 +0800

    key to str helper

--- engine/input/input.h
+++ engine/input/input.h
@@ -6,7 +6,17 @@ namespace aph
 enum class Key
 {
     Unknown,
-    A,
+    _0 = 48,
+    _1,
+    _2,
+    _3,
+    _4,
+    _5,
+    _6,
+    _7,
+    _8,
+    _9,
+    A = 65,
     B,
     C,
     D,
@@ -42,18 +52,47 @@ enum class Key
     Right,
     Up,
     Down,
-    _1,
-    _2,
-    _3,
-    _4,
-    _5,
-    _6,
-    _7,
-    _8,
-    _9,
-    _0,
     Count
 };
+
+inline std::string KeyToStr(Key key)
+{
+    if((key >= Key::A && key <= Key::Z) || (key >= Key::_0 && key <= Key::_9))
+    {
+        char keyChar = static_cast<char>(key);
+        return {1, keyChar};
+    }
+
+    switch(key)
+    {
+    case Key::Unknown:
+        return "Unknown";
+    case Key::Return:
+        return "Return";
+    case Key::LeftCtrl:
+        return "Left Ctrl";
+    case Key::LeftAlt:
+        return "Left Alt";
+    case Key::LeftShift:
+        return "Left Shift";
+    case Key::Space:
+        return "Space";
+    case Key::Escape:
+        return "Escape";
+    case Key::Left:
+        return "Left Arrow";
+    case Key::Right:
+        return "Right Arrow";
+    case Key::Up:
+        return "Up Arrow";
+    case Key::Down:
+        return "Down Arrow";
+    case Key::Count:
+        return "Count";
+    default:
+        return "Invalid Key";
+    }
+}
 
 enum class MouseButton
 {
