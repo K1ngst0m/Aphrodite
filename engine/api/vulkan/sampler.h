cb6844febea47a626b9d31c33ebf708f4c0931ec
kingstom.chen <kingstom.chen@gmail.com>
2025-02-28 11:19:24 +0800

    sampler preset code refine

--- engine/api/vulkan/sampler.h
+++ engine/api/vulkan/sampler.h
@@ -24,6 +24,8 @@ struct SamplerCreateInfo
     float     maxLod        = {};
     float     maxAnisotropy = {};
     bool      immutable     = {};
+
+    SamplerCreateInfo& preset(SamplerPreset preset);
 };
 
 class Sampler : public ResourceHandle<VkSampler, SamplerCreateInfo>
@@ -41,91 +43,3 @@ private:
 };
 
 }  // namespace aph::vk
-
-namespace aph::vk
-{
-inline SamplerCreateInfo samplerCreateInfo(SamplerPreset preset)
-{
-    SamplerCreateInfo ci;
-    ci.maxLod        = ::vk::LodClampNone;
-    ci.maxAnisotropy = 1.0f;
-
-    switch(preset)
-    {
-    case SamplerPreset::NearestShadow:
-    case SamplerPreset::LinearShadow:
-        ci.compareFunc = CompareOp::LessEqual;
-        break;
-    default:
-        break;
-    }
-
-    switch(preset)
-    {
-    case SamplerPreset::TrilinearClamp:
-    case SamplerPreset::TrilinearWrap:
-    case SamplerPreset::DefaultGeometryFilterWrap:
-    case SamplerPreset::DefaultGeometryFilterClamp:
-        ci.mipMapMode = SamplerMipmapMode::Linear;
-        break;
-    default:
-        ci.mipMapMode = SamplerMipmapMode::Nearest;
-        break;
-    }
-
-    switch(preset)
-    {
-    case SamplerPreset::DefaultGeometryFilterClamp:
-    case SamplerPreset::DefaultGeometryFilterWrap:
-    case SamplerPreset::LinearClamp:
-    case SamplerPreset::LinearWrap:
-    case SamplerPreset::TrilinearClamp:
-    case SamplerPreset::TrilinearWrap:
-    case SamplerPreset::LinearShadow:
-        ci.magFilter = Filter::Linear;
-        ci.minFilter = Filter::Linear;
-        break;
-    default:
-        ci.magFilter = Filter::Nearest;
-        ci.minFilter = Filter::Nearest;
-        break;
-    }
-
-    switch(preset)
-    {
-    default:
-    case SamplerPreset::DefaultGeometryFilterWrap:
-    case SamplerPreset::LinearWrap:
-    case SamplerPreset::NearestWrap:
-    case SamplerPreset::TrilinearWrap:
-        ci.addressU = SamplerAddressMode::Repeat;
-        ci.addressV = SamplerAddressMode::Repeat;
-        ci.addressW = SamplerAddressMode::Repeat;
-        break;
-
-    case SamplerPreset::DefaultGeometryFilterClamp:
-    case SamplerPreset::LinearClamp:
-    case SamplerPreset::NearestClamp:
-    case SamplerPreset::TrilinearClamp:
-    case SamplerPreset::NearestShadow:
-    case SamplerPreset::LinearShadow:
-        ci.addressU = SamplerAddressMode::ClampToEdge;
-        ci.addressV = SamplerAddressMode::ClampToEdge;
-        ci.addressW = SamplerAddressMode::ClampToEdge;
-        break;
-    }
-
-    switch(preset)
-    {
-    case SamplerPreset::DefaultGeometryFilterWrap:
-    case SamplerPreset::DefaultGeometryFilterClamp:
-        // TODO limited by device properties
-        ci.maxAnisotropy = 16.0f;
-        break;
-    default:
-        break;
-    }
-
-    return ci;
-}
-}  // namespace aph::vk::init
